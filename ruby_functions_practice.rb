cad96df0f6ae5c58c84032f89b481dd30d679a77
Aline Mokfa <alinemokfa@hotmail.co.uk>
2017-09-26 22:37:32 +0100

    passed number_to_short_month_name test

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -40,3 +40,14 @@ def number_to_full_month_name(number)
     return "September"
    end
 end
+
+def number_to_short_month_name(number)
+  return case number
+  when 1
+    "Jan"
+  when 3
+    "Mar"
+  when 9
+    "Sep"
+  end
+end
